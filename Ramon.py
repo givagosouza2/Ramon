45b0940a1e9ab5cf8b67abc73157becab517c598
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2024-11-12 12:43:08 -0300

    Update Ramon.py

--- Ramon.py
+++ Ramon.py
@@ -28,8 +28,8 @@ if uploaded_file is not None:
         # Plotar o gráfico de estimação
         st.write("Plotando o gráfico de estimação:")
         fig = dabest_data.mean_diff.plot()
-        plt[0].ylim(80, 100)
-        plt[1].ylim(-10, 10)
+        plt[0].set_ylim(80, 100)
+        plt[1].set_ylim(-10, 10)
         st.pyplot(fig)
 else:
     st.info("Por favor, faça o upload de um arquivo CSV.")
