8bbd00674e5298fac51530f87a191c2d8f01ac3d
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2024-11-12 12:17:36 -0300

    Update Ramon.py

--- Ramon.py
+++ Ramon.py
@@ -1,35 +1,23 @@
 import streamlit as st
 import pandas as pd
 import dabest
-import matplotlib.pyplot as plt
 
-# Configurar o título da aplicação
+# Carregar o arquivo CSV
 st.title("Análise de Dados com Dabest")
-
-# Fazer o upload do arquivo CSV
 uploaded_file = st.file_uploader("Faça o upload de um arquivo CSV", type="csv")
 
 if uploaded_file is not None:
     # Ler o arquivo CSV
     data = pd.read_csv(uploaded_file)
 
-    # Exibir os primeiros dados para visualização
-    st.write("Dados carregados:")
-    st.write(data.head())
-
     # Carregar os dados no Dabest
-    try:
-        dabest_data = dabest.load(data=data, x="Trial", y="Angulo", idx=(
-                "Baseline", "T1", "T2", "T3", "T4"))
+    dabest_data = dabest.load(data=data, x="Trial", y="Angulo", idx=(
+            "Baseline", "T1", "T2", "T3", "T4"))
 
-        # Plotar o gráfico de estimação
-        st.write("Plotando o gráfico de estimação:")
-        fig, ax = plt.subplots()
-        dabest_data.mean_diff.plot(ax=ax, raw_ylim=(80, 100),contrast_ylim=(-10, 10))
-
-        # Exibir o gráfico no Streamlit
-        st.pyplot(fig)
-    except Exception as e:
-        st.error(f"Erro ao processar os dados: {e}")
+    # Plotar o gráfico de estimação
+    st.write("Plotando o gráfico de estimação:")
+    fig = dabest_data.mean_diff.plot(raw_ylim=(80, 100),
+    contrast_ylim=(-10, 10))
+    st.pyplot(fig)
 else:
     st.info("Por favor, faça o upload de um arquivo CSV.")
