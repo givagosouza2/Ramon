9ebd9f813d6adc69cf074539eb40cd7e564ed014
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2024-11-12 13:05:18 -0300

    Update Ramon.py

--- Ramon.py
+++ Ramon.py
@@ -23,11 +23,12 @@ if uploaded_file is not None:
 
         # Carregar os dados no DABEST
         dabest_data = dabest.load(data=data, x="Trial", y="Angulo", idx=(
-            "Baseline", "T1", "T2", "T3", "T4"),raw_ylim=(80,100),contrast_ylim = (-10,10))
+            "Baseline", "T1", "T2", "T3", "T4"))
 
         # Plotar o gráfico de estimação
         st.write("Plotando o gráfico de estimação:")
-        fig = dabest_data.mean_diff.plot()
+        fig = plt.figure()
+        plt.plot(dabest_data.mean_diff,raw_ylim=(80,100),contrast_ylim = (-10,10))
        
         st.pyplot(fig)
 else:
